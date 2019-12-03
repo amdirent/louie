86da7ee01f26cbd92f840e25b92fa3ceaea53a24
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2019-12-02 19:42:18 -0600

    Fix type & bump to 0.0.4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export {default as LoginComponent} from './components/LoginComponent';
+export {default as LoginForm} from './components/LoginForm';
 export {default as LoadingComponent} from './components/LoadingComponent';
 export {default as MessageBoxComponent} from './components/MessageBoxComponent';
 export {default as MessageBoxMixin} from './mixins/MessageBoxMixin.js';
