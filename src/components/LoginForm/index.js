999308bb22ba342029b93c3ab5af7dc82e6cf0e5
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2019-12-02 23:19:20 -0600

    Create a default submission handler for LoginForm

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -4,21 +4,27 @@ export default class LoginForm extends React.PureComponent {
   render() {
     const Logo = this.props.logo;
     
-    const submissionHandler = this.props.onSubmit || function(event) {
+    const defaultSubmissionHandler = function(event) {
       if (event) event.preventDefault();
         console.log(this.refs.usernameInput.value);
         console.log(this.refs.passwordInput.value); 
     };
 
+    const onSubmitHandler = this.props.onSubmit 
+      ? this.props.onSubmit 
+      : defaultSubmissionhandler; 
+
     return (
       <div className="login-page">
         { this.props.logo && <Logo /> }
         <div className="form">
-          <form className="login-form" onSubmit={this.props.onSubmit}>
+          <form className="login-form" onSubmit={onSubmitHandler}>
             <input type="text" placeholder="username" ref="usernameInput"/>
             <input type="password" placeholder="password" ref="passwordInput"/>
             <button>login</button>
-            <p className="message">Forgot password? <a href="#">Request a reset.</a></p>
+            <p className="message">
+              Forgot password? <a href="#">Request a reset.</a>
+            </p>
           </form>
         </div>
       </div>
