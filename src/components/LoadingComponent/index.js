ba12f742f67263c7ec1d0519099d688b1582f52d
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2019-12-04 15:37:15 -0600

    Instantly load LoadingComponent with no promises

--- src/components/LoadingComponent/index.js
+++ src/components/LoadingComponent/index.js
@@ -22,16 +22,20 @@ export default class LoadingComponent extends React.PureComponent {
    * 
    * @param {Promise} promise - Promise to retrieve data resolving to new state.
    */
-  componentDidMount(promises=[]) {
-    Promise.all(promises).then((results) => {
-      let newState = {};
+  componentDidMount(promises) {
+    if (!promises) {
+      this.setState({loaded: true});
+    } else {
+      Promise.all(promises).then((results) => {
+        let newState = {};
 
-      for (let i=0; i<=results.length; i++) {
-        newState = {...newState, ...results[i]}
-      }
+        for (let i=0; i<=results.length; i++) {
+          newState = {...newState, ...results[i]}
+        }
 
-      this.setState({...newState, ...{loaded: true}});
-    });
+        this.setState({...newState, ...{loaded: true}});
+      });
+    }
   }
 
   /**
