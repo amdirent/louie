08ead4c012a889f7b12952caf95d415386bd9797
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2019-12-04 17:20:25 -0600

    WIP try to set user in state

--- src/components/AuthenticatedComponent/index.js
+++ src/components/AuthenticatedComponent/index.js
@@ -3,32 +3,35 @@ import LoadingComponent from '../LoadingComponent';
 import Logout from '../Logout';
 import jwtDecode from 'jwt-decode';
 
+const verifyUser = () => {
+    let user;
+
+    try {
+      const authedUser = jwtDecode(sessionStorage.getItem('idToken'));
+      const currentTimestamp = new Date().getTime() / 1000;
+      const expiration = authedUser.exp - 600; // Expired if expiring within 10 mins
+      const isExpired = currentTimestamp >= expiration;
+
+      if (isExpired) {
+        throw "User's session has expired";
+      } else {
+        // TODO: Write code to update the accessToken
+        //resolve({user: user});
+        console.log("^^^^^^^^^^^^^^^ Authed ^^^^^^^^^^^^^^")
+        user = authedUser;
+      }
+    } catch(e) {
+      console.log(e);
+      Logout.logout();
+    }
+
+  return user;
+};
+
 export default class AuthenticatedComponent extends LoadingComponent {
   constructor(props) {
     super(props);
-    console.log(this.state);
-    this.state = Object.assign({}, this.state, { user: 'butter' });
-//let user;
-//    try {
-//      const authedUser = jwtDecode(sessionStorage.getItem('idToken'));
-//      const currentTimestamp = new Date().getTime() / 1000;
-//      const expiration = authedUser.exp - 600; // Expired if expiring within 10 mins
-//      const isExpired = currentTimestamp >= expiration;
-//
-//      if (isExpired) {
-//        throw "User's session has expired";
-//      } else {
-//        // TODO: Write code to update the accessToken
-//        //resolve({user: user});
-//        console.log("^^^^^^^^^^^^^^^ Authed ^^^^^^^^^^^^^^")
-//        user = authedUser;
-//      }
-//    } catch(e) {
-//      console.log(e);
-//      Logout.logout();
-//    }
-//
-//    this.state.user = user;
+    console.log(verifyUser);
   }
 
   //componentDidMount(promises=[]) {
