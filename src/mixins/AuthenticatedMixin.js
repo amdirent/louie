71e3539bc171ce94c20380a08b70d3ee7b9653bb
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2019-12-04 17:50:09 -0600

    Test static state variable

--- src/mixins/AuthenticatedMixin.js
+++ src/mixins/AuthenticatedMixin.js
@@ -1,32 +1,31 @@
-import update from 'immutability-helper';
-import jwtDecode from 'jwt-decode';
-
-const verifyUser = () => {
-  let user;
-
-  try {
-    const authedUser = jwtDecode(sessionStorage.getItem('idToken'));
-    const currentTimestamp = new Date().getTime() / 1000;
-    const expiration = authedUser.exp - 600; // Expired if expiring within 10 mins
-    const isExpired = currentTimestamp >= expiration;
-
-    if (isExpired) {
-      throw "User's session has expired";
-    } else {
-      // TODO: Write code to update the accessToken
-      //resolve({user: user});
-      user = authedUser;
-    }
-  } catch(e) {
-    console.log(e);
-    Logout.logout();
-  }
-
-  return user;
-};
+//import jwtDecode from 'jwt-decode';
+//
+//const verifyUser = () => {
+//  let user;
+//
+//  try {
+//    const authedUser = jwtDecode(sessionStorage.getItem('idToken'));
+//    const currentTimestamp = new Date().getTime() / 1000;
+//    const expiration = authedUser.exp - 600; // Expired if expiring within 10 mins
+//    const isExpired = currentTimestamp >= expiration;
+//
+//    if (isExpired) {
+//      throw "User's session has expired";
+//    } else {
+//      // TODO: Write code to update the accessToken
+//      //resolve({user: user});
+//      user = authedUser;
+//    }
+//  } catch(e) {
+//    console.log(e);
+//    Logout.logout();
+//  }
+//
+//  return user;
+//};
 
 const AuthenticatedMixin = (Base) => class extends Base {
-  state = { user: verifyUser() };
+  state = { user: 'Butter' };
 }
 
 export default AuthenticatedMixin;
