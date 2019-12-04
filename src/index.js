1caec9fbed114c09c615476746963cc1932cc1a8
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2019-12-04 17:43:52 -0600

    Make Authenticated a mixin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 export {default as LoginForm} from './components/LoginForm';
 export {default as Logout} from './components/Logout';
-export {default as AuthenticatedComponent} from './components/AuthenticatedComponent';
+//export {default as AuthenticatedComponent} from './components/AuthenticatedComponent';
 export {default as LoadingComponent} from './components/LoadingComponent';
 export {default as MessageBoxComponent} from './components/MessageBoxComponent';
+
 export {default as MessageBoxMixin} from './mixins/MessageBoxMixin.js';
+export {default as AuthenticatedMixin} from './mixins/AuthenticatedMixin.js';
