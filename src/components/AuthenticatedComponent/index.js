c16a387a94b5f0dfbf0d183cd0c08760a2d7bf03
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2019-12-04 16:41:51 -0600

    Authenticate user in the constructor

--- src/components/AuthenticatedComponent/index.js
+++ src/components/AuthenticatedComponent/index.js
@@ -4,27 +4,49 @@ import Logout from '../Logout';
 import jwtDecode from 'jwt-decode';
 
 export default class AuthenticatedComponent extends LoadingComponent {
-  componentDidMount(promises=[]) {
-    const verifySession = new Promise(function(resolve) {
-      try {
-        const user = jwtDecode(sessionStorage.getItem('idToken'));
-        const currentTimestamp = new Date().getTime() / 1000;
-        const expiration = user.exp - 600; // Expired if expiring within 10 mins
-        const isExpired = currentTimestamp >= expiration;
+  constructor(props) {
+    super(props);
 
-        if (isExpired) {
-          throw "User's session has expired";
-        } else {
-          // TODO: Write code to update the accessToken
-          resolve({user: user});
-        }
-      } catch(e) {
-        console.log(e);
-        Logout.logout();
+    try {
+      const user = jwtDecode(sessionStorage.getItem('idToken'));
+      const currentTimestamp = new Date().getTime() / 1000;
+      const expiration = user.exp - 600; // Expired if expiring within 10 mins
+      const isExpired = currentTimestamp >= expiration;
+
+      if (isExpired) {
+        throw "User's session has expired";
+      } else {
+        // TODO: Write code to update the accessToken
+        //resolve({user: user});
+        this.state.user = user;
       }
-    });
-
-    promises.push(verifySession);
-    super.componentDidMount(promises);
+    } catch(e) {
+      console.log(e);
+      Logout.logout();
+    }
   }
+
+  //componentDidMount(promises=[]) {
+  //  const verifySession = new Promise(function(resolve) {
+  //    try {
+  //      const user = jwtDecode(sessionStorage.getItem('idToken'));
+  //      const currentTimestamp = new Date().getTime() / 1000;
+  //      const expiration = user.exp - 600; // Expired if expiring within 10 mins
+  //      const isExpired = currentTimestamp >= expiration;
+
+  //      if (isExpired) {
+  //        throw "User's session has expired";
+  //      } else {
+  //        // TODO: Write code to update the accessToken
+  //        resolve({user: user});
+  //      }
+  //    } catch(e) {
+  //      console.log(e);
+  //      Logout.logout();
+  //    }
+  //  });
+
+  //  promises.push(verifySession);
+  //  super.componentDidMount(promises);
+  //}
 }
