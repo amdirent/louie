992afeea1d39d845e4a1c5894137af569b8892cb
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2019-12-03 18:01:43 -0600

    Export component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export {default as LoginForm} from './components/LoginForm';
 export {default as Logout} from './components/Logout';
+export {default as AuthenticatedComponent} from './components/AuthenticatedComponent';
 export {default as LoadingComponent} from './components/LoadingComponent';
 export {default as MessageBoxComponent} from './components/MessageBoxComponent';
 export {default as MessageBoxMixin} from './mixins/MessageBoxMixin.js';
