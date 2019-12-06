a1a17b56ad99387bc793513d2791a2f3443a02f0
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2019-12-05 21:12:09 -0600

    Run logout command on construct

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -3,10 +3,10 @@ import React, {PureComponent} from 'react';
 export default class Logout extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.props.auth.logout();
   }
 
-  //componentDidMount() {
-  //  this.props.auth.logout();
-  //}
+  render() {
+    this.props.auth.logout();
+    return null;
+  }
 }
