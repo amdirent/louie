f2bc71cf7d6a524ae495d0e4d978ace9a939ca1b
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2019-12-05 20:28:37 -0600

    Rename ConsumerComponent to AuthConsumerComponent

--- src/auth/AuthConsumerComponent.js
+++ src/auth/AuthConsumerComponent.js
@@ -1,6 +1,6 @@
 import AuthContext from './AuthContext.js';
 
-const ConsumerComponent = (props) => (
+const AuthConsumerComponent = (props) => (
   <AuthContext.Consumer>
     { value => React.cloneElement(props.children, {auth: value}) }
   </AuthContext.Consumer>
