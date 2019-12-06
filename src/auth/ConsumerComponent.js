3fa4f25f65aca1428714df6a7357a32c6b43bd34
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2019-12-05 20:33:42 -0600

    Export AuthConsumerComponent

---
+++ src/auth/ConsumerComponent.js
@@ -0,0 +1,9 @@
+import AuthContext from './AuthContext.js';
+
+const AuthConsumerComponent = (props) => (
+  <AuthContext.Consumer>
+    { value => React.cloneElement(props.children, {auth: value}) }
+  </AuthContext.Consumer>
+);
+
+export default AuthConsumerComponent;
