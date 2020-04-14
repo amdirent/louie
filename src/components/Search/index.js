14e4827ac76bd0da44df482690f54f8ab0d1b1f0
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2020-04-13 22:47:25 -0500

    Add Search input component

---
+++ src/components/Search/index.js
@@ -0,0 +1,67 @@
+import React, {PureComponent} from 'react';
+
+export default class Search extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      onSearchResults: props.onSearchResults,
+      masterList: props.masterList,
+      resultSet: props.resultSet,
+      deleting: false,
+      conf: props.conf
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    return {
+      resultSet: props.resultSet
+    }
+  }
+
+  render() {
+    const {masterList, resultSet, onSearchResults, deleting, conf} = this.state;
+    const {style, type} = this.props;
+
+    const filterResults = (query, useMaster) => {
+      const list = (useMaster|| query.length === 0) ? masterList: resultSet;
+      const results = list.filter(function(q) {
+        if (conf.fields) {
+          let match = false;
+          conf.fields.forEach(f => {
+            const value = q[f];
+            if (value && value.includes(query))
+              match = true;
+          });
+
+          return match;
+        } else {
+          return q.includes(query);
+        }
+      });
+
+      return results;
+    };
+
+    return (
+      <input
+        type={type ? type : 'search'}
+        style={style}
+        placeholder='Search...'
+        onKeyDown={(e) => {
+          let newState = {loading: true};
+
+          if (e.key === 'Backspace')
+            newState.deleting = true;
+
+          this.setState(newState);
+        }}
+        onChange={(e) => {
+          const query = e.target.value;
+          const results = filterResults(query, deleting);
+          this.setState({deleting: false}, () => onSearchResults(results));
+        }}
+      />
+    );
+  }
+}
