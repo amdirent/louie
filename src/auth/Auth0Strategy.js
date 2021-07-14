fcba88b9066e6f3dde055e952d7d9f85fb8af3fd
Christopher Rankin <crankin@Christophers-MacBook-Pro.local>
2021-07-13 19:58:02 -0500

    Improve session handling

--- src/auth/Auth0Strategy.js
+++ src/auth/Auth0Strategy.js
@@ -22,32 +22,55 @@ export default class Auth0Strategy {
     this.getAccessToken = this.getAccessToken.bind(this);
     this.changePassword = this.changePassword.bind(this);
     this.clearSession = this.clearSession.bind(this);
+    this.setDataItem = this.setDataItem.bind(this);
+  }
+
+  setDataItem(key, value) {
+    sessionStorage.setItem(key, value);
+    Cookies.set(key, value);
   }
 
   clearSession() {
     localStorage.clear();
     sessionStorage.clear();
-    Cookies.remove('id_token', {domain: `${process.env.NODE_ENV === 'development' ? '127.0.0.1' : '.rentbutter.com'}`});
-    Cookies.remove('access_token', {domain: `${process.env.NODE_ENV === 'development' ? '127.0.0.1' : '.rentbutter.com'}`});
+
+    Cookies.remove('id_token', {domain: window.location.hostname});
+    Cookies.remove('access_token', {domain: window.location.hostname});
+    Cookies.remove('expiresAt', {domain: window.location.hostname});
+    Cookies.remove('scope', {domain: window.location.hostname});
+    Cookies.remove('role', {domain: window.location.hostname});
+    Cookies.remove('accountId', {domain: window.location.hostname});
+
+    //Cookies.remove('id_token', {domain: `${process.env.NODE_ENV === 'development' ? '127.0.0.1' : '.rentbutter.com'}`});
+    //Cookies.remove('access_token', {domain: `${process.env.NODE_ENV === 'development' ? '127.0.0.1' : '.rentbutter.com'}`});
   }
 
   setSession(authResult, callback) {
-    sessionStorage.setItem('expiresAt', (authResult.expiresIn) + this.getCurrentTimestamp());
-    sessionStorage.setItem('accessToken', authResult.accessToken);
-    sessionStorage.setItem('idToken', authResult.idToken);
-    sessionStorage.setItem('scope', authResult.scope);
-    sessionStorage.setItem('role', jwtDecode(authResult.idToken)['https://rentbutter.com/roles']);
-    sessionStorage.setItem('accountId', jwtDecode(authResult.idToken)['https://rentbutter.com/accountId']);
+    this.setDataItem('accessToken', authResult.accessToken);
+    this.setDataItem('idToken', authResult.idToken);
+    this.setDataItem('expiresAt', (authResult.expiresIn) + this.getCurrentTimestamp());
+    this.setDataItem('scope', authResult.scope);
+    this.setDataItem('role', jwtDecode(authResult.idToken)['https://rentbutter.com/roles']);
+    this.setDataItem('accountId', jwtDecode(authResult.idToken)['https://rentbutter.com/accountId']);
 
-    Cookies.set('id_token', authResult.idToken);
+//    sessionStorage.setItem('accessToken', authResult.accessToken);
+//    sessionStorage.setItem('idToken', authResult.idToken);
+//    sessionStorage.setItem('expiresAt', (authResult.expiresIn) + this.getCurrentTimestamp());
+//    sessionStorage.setItem('scope', authResult.scope);
+//    sessionStorage.setItem('role', jwtDecode(authResult.idToken)['https://rentbutter.com/roles']);
+//    sessionStorage.setItem('accountId', jwtDecode(authResult.idToken)['https://rentbutter.com/accountId']);
+//
+//    Cookies.set('id_token', authResult.idToken);
 
     if (callback) callback();
   }
 
   logout() {
-    sessionStorage.clear();
-    Cookies.remove('id_token', {domain: `${process.env.NODE_ENV === 'development' ? '127.0.0.1' : '.rentbutter.com'}`});
-    Cookies.remove('access_token', {domain: `${process.env.NODE_ENV === 'development' ? '127.0.0.1' : '.rentbutter.com'}`});
+    //sessionStorage.clear();
+    //Cookies.remove('id_token', {domain: `${process.env.NODE_ENV === 'development' ? '127.0.0.1' : '.rentbutter.com'}`});
+    //Cookies.remove('access_token', {domain: `${process.env.NODE_ENV === 'development' ? '127.0.0.1' : '.rentbutter.com'}`});
+
+    this.clearSession();
     this.auth0.logout({returnTo: process.env.LOGIN_URL});
   }
 
@@ -75,28 +98,34 @@ export default class Auth0Strategy {
   }
 
   refreshToken(callback, errback) {
-   this.auth0.checkSession(
-     {},
-     function(err, authResult) {
-      if (err) {
-          if (errback)
+    this.auth0.checkSession(
+      {
+        audience: process.env.AUTH0_API_AUDIENCE,
+        scope: process.env.AUTH0_SCOPE
+      },
+      function(err, authResult) {
+        if (err) {
+          if (errback) {
             errback(err);
+          } else {
+            console.log(err);
+          }
         } else {
-          that.setSession(authResult, callback);
+          this.setSession(authResult, callback);
         }
-     }
-   ); 
+      }
+    );
   }
 
   verifySession() {
     let user;
-  
+
     try {
       const authedUser = this.getUser();
       const currentTimestamp = new Date().getTime() / 1000;
       const expiration = authedUser.exp - 600; // Expired if expiring within 10 mins
       const isExpired = currentTimestamp >= expiration;
-  
+
       if (isExpired) {
         throw "User's session has expired";
       } else {
@@ -113,15 +142,15 @@ export default class Auth0Strategy {
 
   getUser() {
     try {
-      const token = sessionStorage.getItem('idToken') || Cookies.get('id_token'); 
+      const token = sessionStorage.getItem('idToken') || Cookies.get('id_token');
       return jwtDecode(token);
     } catch(e) {
-      this.logout();      
+      this.logout();
     }
   }
 
   getAccessToken() {
-    const token = sessionStorage.getItem('accessToken') || Cookies.get('access_token'); 
+    const token = sessionStorage.getItem('accessToken') || Cookies.get('access_token');
     return token;
   }
 
